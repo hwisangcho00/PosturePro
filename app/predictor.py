aca5d33fd4de994b85656ddc127ffe35c737c012
gabelip <113263520+gabelip@users.noreply.github.com>
2025-03-23 15:54:30 -0400

    model training and prediction

---
+++ app/predictor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import joblib
+
+model = joblib.load('ml_model/posture_classifier.pkl')
+
+def predict_posture(input_data):
+    """
+    Predicts the posture classification based on input data.
+    :param input_data: DataFrame containing the input features.
+    :return: Predicted class label.
+    """
+    prediction = model.predict(input_data)
+    return prediction
