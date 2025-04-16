b9f7035b5669c22736f0592fbdc07d69d836797b
hwisangcho00 <hwisangcho00@gmail.com>
2025-04-16 18:26:09 -0400

    no hardware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import users, workouts, metrics, hardware  # Import both users and workouts routers
+from app.routers import users, workouts, metrics  # Import both users and workouts routers
 from app.database import Base, engine
 
 # Initialize database tables
@@ -16,7 +16,3 @@ async def read_root():
 app.include_router(users.router, prefix="/api/users", tags=["Users"])
 app.include_router(workouts.router, prefix="/api/workouts", tags=["Workouts"])
 app.include_router(metrics.router, prefix="/api/metrics", tags=["Metrics"])
-
-
-# app.include_router(metrics.router, prefix="/api/metrics", tags=["Metrics"])
-# app.include_router(hardware.router, prefix="/api/hardware", tags=["Hardware"])
